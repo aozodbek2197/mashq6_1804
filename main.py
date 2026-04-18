842d51766cd880211589db3fffbfb3d5c80bd93f
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-18 12:36:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1-mashq
+d = {}
+d["name"] = "Ali"
+print(d)
+# 2-mashq
+s = {1,2,3,3}
+print(s)
+# 3-mashq
+s = {1,2}
+s.add(3)
+print(s)
+# 4-mashq
+t = (1,2,3)
+print(t[0])
+# 5-mashq
+a, b = (5, 10)
+print(a, b)
